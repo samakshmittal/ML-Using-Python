0c9f125ec88cf409aaf2ea3c08b5b008662ac5b1
samakshmittal <samakshmittal826@gmail.com>
2023-05-25 21:34:26 +0530

    Delete groupby_using_reset_index1.py

--- Chapter 2/groupby_using_reset_index1.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-ipl_auction_df=pd.read_csv("IPL IMB381IPL2013.csv")
-soldprice_by_age_role=ipl_auction_df.groupby(['AGE', 'PLAYING ROLE'])['SOLD PRICE'].mean().reset_index()
-print(soldprice_by_age_role)
