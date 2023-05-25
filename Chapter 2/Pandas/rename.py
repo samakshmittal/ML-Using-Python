fa13fadb32c8fde6badfe91547f8c9feafb1809d
samakshmittal <samakshmittal826@gmail.com>
2023-05-25 21:36:16 +0530

    Create rename.py

---
+++ Chapter 2/Pandas/rename.py
@@ -0,0 +1,7 @@
+import pandas as pd
+ipl_auction_df=pd.read_csv("IPL IMB381IPL2013.csv")
+soldprice_by_age=ipl_auction_df.groupby('AGE')['SOLD PRICE'].mean().reset_index()
+soldprice_by_age_role=ipl_auction_df.groupby(['AGE', 'PLAYING ROLE'])['SOLD PRICE'].mean().reset_index()
+soldprice_comparison=soldprice_by_age_role.merge(soldprice_by_age, on='AGE', how='outer')
+soldprice_comparison.rename(columns={'SOLD PRICE_x':'SOLD_PRICE_AGE_ROLE', 'SOLD PRICE_y':'SOLD_PRICE_AGE'}, inplace=True)
+print(soldprice_comparison.head(5))
