0ab0e826b5e06b6eef37da9cba9e0fb0a7ca62f2
samakshmittal <samakshmittal826@gmail.com>
2023-05-26 10:28:42 +0530

    Create comparing_distributions.py

---
+++ Chapter 2/Matplotlib and Seaborn/comparing_distributions.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sn
+ipl_auction_df=pd.read_csv("IPL IMB381IPL2013.csv")
+soldprice_by_age=ipl_auction_df.groupby('AGE')['SOLD PRICE'].mean().reset_index()
+soldprice_by_age_role=ipl_auction_df.groupby(['AGE', 'PLAYING ROLE'])['SOLD PRICE'].mean().reset_index()
+soldprice_comparison=soldprice_by_age_role.merge(soldprice_by_age, on='AGE', how='outer')
+soldprice_comparison.rename(columns={'SOLD PRICE_x':'SOLD_PRICE_AGE_ROLE', 'SOLD PRICE_y':'SOLD_PRICE_AGE'}, inplace=True)
+sn.distplot(ipl_auction_df[ipl_auction_df['CAPTAINCY EXP']==1]['SOLD PRICE'], color='y', label='Captaincy Experience')
+sn.distplot(ipl_auction_df[ipl_auction_df['CAPTAINCY EXP']==0]['SOLD PRICE'], color='r', label='No Captaincy Experience')
+plt.legend()
+plt.show()
