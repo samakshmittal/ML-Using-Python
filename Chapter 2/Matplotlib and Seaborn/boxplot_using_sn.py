f68fdc2f34c37d158b3c7c62ad6ba8f039b24734
samakshmittal <samakshmittal826@gmail.com>
2023-05-26 10:10:22 +0530

    Create boxplot_using_sn.py

---
+++ Chapter 2/Matplotlib and Seaborn/boxplot_using_sn.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sn
+ipl_auction_df=pd.read_csv("IPL IMB381IPL2013.csv")
+soldprice_by_age=ipl_auction_df.groupby('AGE')['SOLD PRICE'].mean().reset_index()
+soldprice_by_age_role=ipl_auction_df.groupby(['AGE', 'PLAYING ROLE'])['SOLD PRICE'].mean().reset_index()
+soldprice_comparison=soldprice_by_age_role.merge(soldprice_by_age, on='AGE', how='outer')
+soldprice_comparison.rename(columns={'SOLD PRICE_x':'SOLD_PRICE_AGE_ROLE', 'SOLD PRICE_y':'SOLD_PRICE_AGE'}, inplace=True)
+sn.boxplot(ipl_auction_df['SOLD PRICE'])
+plt.show()
