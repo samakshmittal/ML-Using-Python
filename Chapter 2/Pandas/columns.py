839be5627dec38a2c1e89b08c4a18b724d8b1387
samakshmittal <samakshmittal826@gmail.com>
2023-05-26 10:09:43 +0530

    Create columns.py

---
+++ Chapter 2/Pandas/columns.py
@@ -0,0 +1,4 @@
+import pandas as pd
+autos=pd.read_csv("auto-mpg.data", sep='\s+', header=None)
+autos.columns=['mpg', 'cylinders', 'displacement', 'horsepower', 'weight', 'acceleration', 'year', 'origin', 'name']
+print(autos.head(5))
